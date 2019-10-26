98017bc41d9e19a6381c8d8ced9188e3ab1c4a33
hamadakafu <kafu.h1998@gmail.com>
2019-10-27 00:16:57 +0900

    [Fix] sample.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,15 +14,17 @@ git clone https://$INPUT_ACCESS_TOKEN@github.com/$INPUT_REPOSITORY
 dirname=$(echo -n $INPUT_REPOSITORY | awk -F/ '{print $2}')
 cd $dirname 
 
+git remote set-url --push origin https://$INPUT_USERNAME:$INPUT_ACCESS_TOKEN@github.com/$INPUT_REPOSITORY
+
 if [ -n "$INPUT_REFS" ]; then
     git checkout $INPUT_REFS
 else
     git checkout master
 fi
 
-git remote set-url --push origin https://$INPUT_USERNAME:$INPUT_ACCESS_TOKEN@github.com/$INPUT_REPOSITORY
 
 sh -c "$INPUT_COMMAND"
 
-git commit -am "$INPUT_COMMIT_MESSAGE"
+git add .
+git commit -m "$INPUT_COMMIT_MESSAGE"
 git push
